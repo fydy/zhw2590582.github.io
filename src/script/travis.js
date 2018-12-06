d95f55b255eac25915d141b6f814b58e577f0413
Harvey Zack <laozhaochaguan@gmail.com>
2018-12-06 16:27:20 +0800

    Update travis.js

--- src/script/travis.js
+++ src/script/travis.js
@@ -5,10 +5,10 @@ const { getConfig } = require("./utils");
 const config = getConfig();
 const themeDist = path.join(process.cwd(), "src/theme", config.theme);
 exec(`cd ${themeDist} && npm install`, function (err, stdout, stderr) {
-    if (err) logger.fatal(err);
-    console.log(stdout);
+    // if (err) logger.fatal(err);
+    // console.log(stdout);
     exec('npm run build', function (err, stdout, stderr) {
-        if (err) logger.fatal(err);
-        console.log(stdout);
+        // if (err) logger.fatal(err);
+        // console.log(stdout);
     });
 });
