d5bfec053f0689e276d316fdbb922e014541c0b3
Harvey Zack <laozhaochaguan@gmail.com>
2018-12-06 16:50:37 +0800

    Update build.js

--- src/script/build.js
+++ src/script/build.js
@@ -1,7 +1,9 @@
+console.log('Build start...')
 const webpack = require("webpack");
 const logger = require("./logger");
 const webpackConfig = require("./webpack.config.js");
 webpack(webpackConfig, (err, stats) => {
     if (err) logger.fatal(err);
-    console.log(stats)
+    console.log(stats);
+    console.log('Build end')
 });
