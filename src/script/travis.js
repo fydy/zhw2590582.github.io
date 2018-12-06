6723d465b06a701d733e85672a9732ac81a83171
Harvey Zack <laozhaochaguan@gmail.com>
2018-12-06 18:23:05 +0800

    update

--- src/script/travis.js
+++ src/script/travis.js
@@ -6,5 +6,6 @@ const config = getConfig();
 const themeDist = path.join(process.cwd(), "src/theme", config.theme);
 exec(`cd ${themeDist} && npm install`, function (err, stdout, stderr) {
     if (err) logger.fatal(err);
+    exec('ls');
     console.log('The theme\'s dependencies are successfully installed');
 });
