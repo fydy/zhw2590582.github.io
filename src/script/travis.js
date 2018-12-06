8a8537c10e04aa64265774cc902ee07c55d19775
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-06 10:44:05 +0000

    Deploy zhw2590582/zhw2590582.github.io to github.com/zhw2590582/zhw2590582.github.io.git:master

--- src/script/travis.js
+++ src/script/travis.js
@@ -4,7 +4,7 @@ const logger = require("./logger");
 const { getConfig } = require("./utils");
 const config = getConfig();
 const themeDist = path.join(process.cwd(), "src/theme", config.theme);
-exec(`cd ${themeDist} && npm install`, function (err, stdout, stderr) {
+exec(`cd ${themeDist} && ls && npm install && ls`, function (err, stdout, stderr) {
     if (err) logger.fatal(err);
     console.log(stdout);
 });
